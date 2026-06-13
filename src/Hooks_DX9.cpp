f3a2a995fcae2b8be98e18440150471e15de9c0c
OneUp <oneup03@gmail.com>
2026-06-13 01:06:23 -0500

    Add option to disable multithreaded D3D9

--- src/Hooks_DX9.cpp
+++ src/Hooks_DX9.cpp
@@ -1082,7 +1082,8 @@ static HRESULT __stdcall Hooked_CreateDevice(IDirect3D9* This,
         KLOG(L"  game focus HWND=%p\n", g_gameFocusHwnd);
     }
 
-    BehaviorFlags |= D3DCREATE_MULTITHREADED;
+    if (g_config.force_multithreaded_device)
+        BehaviorFlags |= D3DCREATE_MULTITHREADED;
 
     HRESULT hr = pOrigCreateDevice(This, Adapter, DeviceType, hFocusWindow,
                                    BehaviorFlags, pPresentationParameters,
